8b3a77d8d167b8a324057b7df6c200216181ec4e
Prajwal Bakshi <prajwalbakshi@gmail.com>
2020-10-31 21:51:13 +0530

    Moved set_pixel(row, col, r, g, b) to private.

--- Ray Tracer/frame_buffer.h
+++ Ray Tracer/frame_buffer.h
@@ -24,7 +24,7 @@ class frame_buffer {
                 height_(height),
                 channels_(3),
                 asp_((float) width / height),
-                data_((float*)malloc(channels_* width_* height_ * sizeof(float))) {}
+                data_((float*)malloc(double(channels_) * double(width_) * double(height_) * double(sizeof(float)))) {}
     
         ~frame_buffer() {
             free(data_);
@@ -33,26 +33,11 @@ class frame_buffer {
         void set_pixel(
             const int row,
             const int col,
-            const float r,
-            const float g,
-            const float b) {
+            const float3& color,
+            const float factor) {
     
-            //To accomodate (0, 0) being the bottom ...
-            //left corner rather than top right.
-            const int adjusted_row = height_ - 1 - row;
-    
-            const int idx = channels_ * (adjusted_row * width_ + col);
-            data_[idx + 0] = r;
-            data_[idx + 1] = g;
-            data_[idx + 2] = b;
-        }
-    
-        void set_pixel(
-            const int row,
-            const int col,
-            const float3& color) {
-    
-            set_pixel(row, col, color.x(), color.y(), color.z());
+            float3 temp_color = clamp(color * factor, 0.0f, 255.0f);
+            set_pixel(row, col, temp_color.x(), temp_color.y(), temp_color.z());
             
         }
     
@@ -85,6 +70,24 @@ class frame_buffer {
 
         const float asp() const {
             return asp_;
+        }
+
+    private:
+        void set_pixel(
+            const int row,
+            const int col,
+            const float r,
+            const float g,
+            const float b) {
+
+            //To accomodate (0, 0) being the bottom ...
+            //left corner rather than top right.
+            const int adjusted_row = height_ - 1 - row;
+
+            const int idx = channels_ * (adjusted_row * width_ + col);
+            data_[idx + 0] = r;
+            data_[idx + 1] = g;
+            data_[idx + 2] = b;
         }
     
     private:
