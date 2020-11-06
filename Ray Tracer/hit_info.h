4c3c481851733864d3fae7973361bbe2844c702c
Prajwal Bakshi <prajwalbakshi@gmail.com>
2020-11-07 00:11:41 +0530

    Added material properties for lighting.

--- Ray Tracer/hit_info.h
+++ Ray Tracer/hit_info.h
@@ -3,10 +3,13 @@
 
 struct hit_info {
 	public:
+		bool front_face_;
 		float t_;
 		float3 poc_;
 		float3 normal_;
-		bool front_face_;
+		float3 albedo_;				//diffuse coeffecient
+		float3 reflectance_;		//reflectance coeffecient
+		float3 ambient_coeff_;
 
 	public:
 
