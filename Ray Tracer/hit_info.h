dd5edab6424890d5c07ac1be13be86fdbae1a167
Prajwal Bakshi <prajwalbakshi@gmail.com>
2020-11-09 19:16:08 +0530

    Removed recomputation of normal as it is already corrected.

--- Ray Tracer/hit_info.h
+++ Ray Tracer/hit_info.h
@@ -10,14 +10,4 @@ struct hit_info {
 		float3 albedo_;				//diffuse coeffecient
 		float3 reflectance_;		//reflectance coeffecient
 		float3 ambient_coeff_;
-
-	public:
-
-		//Normals always face outwards from the surface ...
-		//Can be against the ray or with the ray.
-		const void correct_normal(const float3& ray_direction) {
-			
-			front_face_ = ray_direction * normal_ < 0.0f;
-			normal_ = front_face_ ? normal_ : -normal_;
-		}
 };
